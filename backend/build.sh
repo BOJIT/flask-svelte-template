4d5cf806be6f3622b6009210b2d55c2a50c583cc
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-01-22 22:32:41 +0000

    Add starting service files

--- backend/build.sh
+++ backend/build.sh
@@ -3,10 +3,14 @@
 # Copy over python scripts
 cp -a ./src/. ./dist/
 
+# Generate requirements.txt for deployment
+pipenv lock -r > ./dist/requirements.txt
+
+# Move deploy files to root directory
+mv ./dist/deploy/* ./dist/
+
 # Delete unused files
 rm ./dist/build.sh
 rm ./dist/Dockerfile
-
-# Create installer file
-touch ./dist/install.sh
-chmod +x ./dist/install.sh
+rm ./dist/Pipfile
+rmdir ./dist/deploy
