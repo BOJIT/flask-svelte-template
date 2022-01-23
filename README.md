a06ee4c6f597d7e82e6ebc4aab0897f796733f4c
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-01-23 01:38:21 +0000

    Build seems to behave now

--- README.md
+++ README.md
@@ -22,8 +22,12 @@ docker-compose --profile prod up
 
 ## Example Deployment (Debian, Raspberry Pi)
 ```bash
-scp -r dist/app pi@192.168.1.203:/home/pi/
-# ssh pi@HOSTNAME -f 'chmod +x ./dist/install.sh && ./dist/install.sh'
+scp -r dist/app pi@HOSTNAME:/home/pi/
+
+ssh pi@HOSTNAME
+cd ./app
+chmod +x install.sh
+sudo ./install.sh
 ```
 
 Note that the `app.service` file and `install.sh` are set up for deploying on a Raspberry Pi. Every other part of the `dist/` output is platform-agnostic
--- backend/deploy/install.sh
+++ backend/deploy/install.sh
@@ -3,15 +3,18 @@
 apt update -y && apt upgrade -y
 
 # Install nginx if it hasn't already been setup
-apt install nginx
+apt install nginx -y
 
 # Install pip and pipenv
-apt install python3-pip
+apt install python3-pip -y
 pip3 install pipenv
 pipenv install --deploy
 
+echo "Installing App on System: this only needs to be done once!"
+
 # Link static files to web directory
-ln -s /var/www/html /home/pi/app/static
+rm -rf /var/www/html
+ln -s /home/pi/app/static /var/www/html
 
 # Install and enable Gunicorn backend service
 cp app.service /etc/systemd/system/
